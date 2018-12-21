713ecfe3860887f75face11754470eda0368be21
katrinLuna <kate.nik2017@yandex.ru>
2018-12-21 16:12:16 +0300

    #2 map in process - input click bag

--- js/map.js
+++ js/map.js
@@ -2,17 +2,33 @@
 
 (function () {
   var mapContainerElement = document.querySelector('.map-container');
-  var pickPoint = document.querySelector('label[for="pickup-point-1"]');
+  var pickPointContainer = document.querySelector('fieldset > div[role="radiogroup"]');
+  var adressCollection = {
+    'pickup-point-1': 'https://yandex.ru/map-widget/v1/-/CBFtJ2hi3C',
+    'pickup-point-2': 'https://yandex.ru/map-widget/v1/-/CBFx7WQSCD'
+  };
 
-  var createMap = function (srcAdress) {
-    var mapIframe = window.utils.createNewNode('iframe', {'width': '100%', 'height': '480', 'allowfullscreen': 'true', 'src': srcAdress}
+  var createMap = function (srcMap) {
+    var mapIframe = window.utils.createNewNode('iframe',
+        {
+          'width': '100%',
+          'height': '480',
+          'allowfullscreen':
+          'true',
+          'src': srcMap
+        }
     );
     return mapIframe;
   };
 
   // map for pickup method
-  pickPoint.addEventListener('click', function () {
-    mapContainerElement.appendChild(createMap('https://yandex.ru/map-widget/v1/-/CBFtJ2hi3C'));
+  pickPointContainer.addEventListener('click', function (evt) {
+
+    if (evt.target.tagName === 'label') {
+      console.log('hi');
+    }
+    //переделать с input change, иначе ловлю 2 события по связке label-input
+    mapContainerElement.appendChild(createMap(adressCollection[evt.target.id]));
   });
 })();
 
