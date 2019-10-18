73edcb4d18a05d9d5cfbd1160eb1c3bfe00d020e
katrinLuna <kate.nik2017@yandex.ru>
2019-10-18 11:08:29 +0300

    add showing mess func

--- sky-test/script.js
+++ sky-test/script.js
@@ -2,26 +2,60 @@
 /**
  * @type {HTMLInputElement}
  */
-var newMessValue = document.querySelector('.add-new-mess__input');
-var messNodeList = document.querySelector('.show-mess__list');
-var createMessElement = document.querySelector('.add-new-mess__btn');
+var newMessValueElement = document.querySelector('.add-new-mess__input');
+var messNodeListElement = document.querySelector('.show-mess__list');
+var createMessBtnElement = document.querySelector('.add-new-mess__btn');
+/**
+ * @type {HTMLInputElement}
+ */
+var showMessLineElement = document.querySelector('.show-mess__line');
+/**
+ * @type {Array}
+ */
 var messArray = [];
+var indexOfMes = 0;
 
 var createNewMess = function (value) {
     var messItem = document.createElement('p');
     messItem.classList.add('show-mess__item');
     messItem.innerText = value;
-    messNodeList.appendChild(messItem);
+    messNodeListElement.appendChild(messItem);
     messArray.push({text: value,
-        element: messItem,
-        isDeleted: false
+                    nodeLink: messItem,
+                    isDeleted: false,
+                    isShow: true
     });
 }
 
-createMessElement.addEventListener("click", function() {
-    if (newMessValue.value.length > 0) {
-        createNewMess(newMessValue.value);
+createMessBtnElement.addEventListener("click", function() {
+    if (newMessValueElement.value.length > 0) {
+        createNewMess(newMessValueElement.value);
     }
-    newMessValue.value = '';
+    newMessValueElement.value = '';
 });
+
+var showingMess = function (index) {
+    showMessLineElement.value = messArray[index].text;
+
+    if (messArray.length - 1 > index) {
+        indexOfMes += 1;
+    } else if (messArray.length - 1 == index) {
+        indexOfMes = 0;
+    }
+};
+
+setInterval( function() {
+    if (messArray.length > 0) {
+        showingMess(indexOfMes);
+    }
+}, 3000);
+
+
+
+
+//надо на messArr сделать setinterval для показа сообщений у которых флаг isShow равен true
+
+//delete-btn на этом узле обработчик событий - 
+// по которому isShow у элемента становится false
+// и для ноды рисуется крестик по флагу тоже 
 
